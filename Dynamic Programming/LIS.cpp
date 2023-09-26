2a0cf262a7aebeb6e47e8e1c5cd7d58b7de9b21f
tsunN <dduy193@gmail.com>
2023-09-27 04:07:06 +0700

    udpate LIS

--- Dynamic Programming/LIS.cpp
+++ Dynamic Programming/LIS.cpp
@@ -2,15 +2,9 @@
 using namespace std; 
 
 int n;
-int a[100005];
 
-signed main() {
-    cin >> n;
-    for(int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
+int LIS1(vector<int> &a) {
     vector<int> dp(n, 1);
-
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < i; j++) {
             if(a[j] < a[i]) {
@@ -18,8 +12,36 @@ signed main() {
             }
         }
     }
-    cout << *max_element(dp.begin(), dp.end());
-    return 0;
+    return *max_element(dp.begin(), dp.end());
+}
+// Time complexity: O(n^2)
+
+int LIS2(vector<int> &a) {
+    vector<int> dp(n + 1, INT_MAX);
+    dp[0] = INT_MIN;
+    for(int i = 0; i < n; i++) {
+        int j = upper_bound(dp.begin(), dp.end(), a[i]) - dp.begin();
+        if(dp[j - 1] < a[i] && a[i] < dp[j]) {
+            dp[j] = a[i];
+        }
+    }
+    int ans = 0;
+    for(int i = 0; i <= n; i++) {
+        if(dp[i] != INT_MAX) {
+            ans = i;
+        }
+    }
+    return ans;
 }
 
-// Time complexity: O(n^2)
+signed main() {
+    cin >> n;
+    vector<int> a(n);
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+    cout << LIS1(a) << "\n";
+    cout << LIS2(a);
+    return 0;
+}
--- Dynamic Programming/LIS.py
+++ Dynamic Programming/LIS.py
@@ -1,15 +1,35 @@
 import math
+from bisect import bisect_right
 
 n = int(input())
 a = list(map(int, input().split()))
 
-dp = [1] * n
-
-for i in range(n):
-    for j in range(i):
-        if a[i] > a[j]:
-            dp[i] = max(dp[i], dp[j] + 1)
-
-print(max(dp))
-
+def LIS(a):
+    n = len(a)
+    dp = [1] * n
+    for i in range(n):
+        for j in range(i):
+            if a[i] > a[j]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return max(dp)
 # Time Complexity: O(n^2)
+
+def LIS2(a):
+    n = len(a)
+    dp = [math.inf] * n
+    dp[0] = 0
+    for i in range(n):
+       j = bisect_right(dp, a[i])
+       if dp[j - 1] < a[i] < dp[j]:
+           dp[j] = a[i]
+    ans = 0
+    for i in range(n):
+        if dp[i] < math.inf:
+            ans = i
+
+    return ans 
+
+
+
+print(LIS(a))
+print(LIS2(a))
